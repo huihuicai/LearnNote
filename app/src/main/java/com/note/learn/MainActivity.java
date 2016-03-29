fcef6a52ff6e97c4b82c7a57c11f1c04aad730b6
unknown <wanghui@foundercomics.com>
2016-03-30 00:12:36 +0800

    首页框架

--- app/src/main/java/com/note/learn/MainActivity.java
+++ app/src/main/java/com/note/learn/MainActivity.java
@@ -1,51 +1,107 @@
 package com.note.learn;
 
+import android.graphics.SweepGradient;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.ecloud.pulltozoomview.PullToZoomScrollViewEx;
+import com.note.learn.utils.ScreenUtil;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
+public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
+
+    private FrameLayout mContentLayout;
+    private TextView mTvCountDown, mTvCurrentDay, mTvCurrentMonth, mTvCurrentWeek, mTvWipeCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-            }
-        });
+        initWidget();
+        initDate();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    private void initWidget() {
+        ((RadioGroup) findViewById(R.id.tab_layout)).setOnCheckedChangeListener(this);
+        PullToZoomScrollViewEx scrollViewEx = (PullToZoomScrollViewEx) findViewById(R.id.pull_zoom_scroll);
+        LinearLayout.LayoutParams localObject = new LinearLayout.LayoutParams(ScreenUtil.getScreenWidth(this), ScreenUtil.dip2px(this, 200f));
+        scrollViewEx.setHeaderLayoutParams(localObject);
+
+        View header = LayoutInflater.from(this).inflate(R.layout.pull_zoom_header, null, false);
+        View content = LayoutInflater.from(this).inflate(R.layout.pull_zoom_content, null, false);
+        View zoomView = LayoutInflater.from(this).inflate(R.layout.pull_zoom_view, null, false);
+        scrollViewEx.setHeaderView(header);
+        scrollViewEx.setZoomView(zoomView);
+        scrollViewEx.setScrollContentView(content);
+
+
+        mContentLayout = (FrameLayout) content.findViewById(R.id.content_layout);
+        mTvCountDown = (TextView) header.findViewById(R.id.tv_header_count_down);
+        mTvCurrentDay = (TextView) header.findViewById(R.id.tv_current_day);
+        mTvCurrentMonth = (TextView) header.findViewById(R.id.tv_current_month);
+        mTvCurrentWeek = (TextView) header.findViewById(R.id.tv_current_week);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void initDate() {
+        final Calendar c = Calendar.getInstance();
+        c.setTimeZone(TimeZone.getTimeZone("GMT+8:00"));
+        int month = c.get(Calendar.MONTH) + 1;
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        int week = c.get(Calendar.DAY_OF_WEEK);
+        String way = "星期天";
+        switch (week) {
+            case 1:
+                way = "星期天";
+                break;
+            case 2:
+                way = "星期一";
+                break;
+            case 3:
+                way = "星期二";
+                break;
+            case 4:
+                way = "星期三";
+                break;
+            case 5:
+                way = "星期四";
+                break;
+            case 6:
+                way = "星期五";
+                break;
+            case 7:
+                way = "星期六";
+                break;
         }
+        mTvCurrentWeek.setText(way);
+        mTvCurrentDay.setText(day < 10 ? ("0" + day) : String.valueOf(day));
+        mTvCurrentMonth.setText(month < 10 ? ("0" + month+"月") : String.valueOf(month)+"月");
 
-        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch (checkedId) {
+            case R.id.rb_record:
+                break;
+            case R.id.rb_analysis:
+                break;
+            case R.id.rb_mine:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/note/learn/utils/ScreenUtil.java
@@ -0,0 +1,53 @@
+package com.note.learn.utils;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+
+/**
+ * Created by wanghui on 2016/3/29.
+ */
+public class ScreenUtil {
+
+    public static Point getScreenSize(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
+        Point size = new Point(outMetrics.widthPixels, outMetrics.heightPixels);
+        return size;
+    }
+
+    public static int getScreenWidth(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
+        return outMetrics.widthPixels;
+    }
+
+    /**
+     * 获得屏幕宽度
+     *
+     * @param context
+     * @return
+     */
+    public static int getScreenHeight(Context context) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(outMetrics);
+        return outMetrics.heightPixels;
+    }
+
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+}
