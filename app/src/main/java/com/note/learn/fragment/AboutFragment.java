90e38931432a87efbfc28d1c346be3ed5374a9fa
unknown <wanghui@foundercomics.com>
2016-04-06 22:58:35 +0800

    关于页面

--- app/src/main/java/com/note/learn/fragment/AboutFragment.java
+++ app/src/main/java/com/note/learn/fragment/AboutFragment.java
@@ -1,20 +1,52 @@
 package com.note.learn.fragment;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.note.learn.R;
 
 /**
  * Created by wanghui on 2016/3/31.
  */
 public class AboutFragment extends Fragment {
 
+    private TextView mTvVersion;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+        mTvVersion = (TextView) view.findViewById(R.id.tv_version_code);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        showVersion();
+    }
+
+    private void showVersion() {
+        PackageManager packageManager = getActivity().getPackageManager();
+        String packageName = getActivity().getPackageName();
+        int flags = 0;
+        PackageInfo packageInfo = null;
+        try {
+            packageInfo = packageManager.getPackageInfo(packageName, flags);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        if (packageInfo != null) {
+            String versionName = packageInfo.versionName;
+            mTvVersion.setText("当前版本v" + versionName);
+        }
     }
 }
