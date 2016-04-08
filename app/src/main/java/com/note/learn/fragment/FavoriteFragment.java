2e8ebad5df04693f7897e4bebd01cb6164793276
unknown <1270174074@qq.com>
2016-04-08 17:42:25 +0800

    日历的view

--- app/src/main/java/com/note/learn/fragment/FavoriteFragment.java
+++ app/src/main/java/com/note/learn/fragment/FavoriteFragment.java
@@ -7,14 +7,30 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.note.learn.R;
+import com.note.learn.view.CalendarView;
+
+import java.util.Calendar;
+
 /**
  * Created by wanghui on 2016/3/31.
  */
 public class FavoriteFragment extends Fragment {
 
+    private CalendarView mCalendarView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_favorite, container, false);
+        mCalendarView = (CalendarView) view.findViewById(R.id.view_calendar);
+        mCalendarView.setOnDataChangeListener(new CalendarView.OnDataChangeListener() {
+            @Override
+            public void dataChange(int year, int month) {
+                mCalendarView.setCalendarData(year,month,null);
+            }
+        });
+        return view;
     }
+
+
 }
--- app/src/main/java/com/note/learn/view/CalendarView.java
+++ app/src/main/java/com/note/learn/view/CalendarView.java
@@ -10,43 +10,112 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.FrameLayout;
 import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.note.learn.R;
 import com.note.learn.utils.ToolUtil;
 
 import java.util.Calendar;
+import java.util.TimeZone;
 
 /**
  * Created by wanghui on 2016/3/31.
  */
-public class CalendarView extends FrameLayout {
+public class CalendarView extends FrameLayout implements View.OnClickListener {
 
-    private LayoutInflater mInflater;
     private GridView mGridView;
+    private TextView mTvCalendarDate;
+    private ImageButton mBtnLeft, mBtnRight;
     private CalendarAdapter mAdapter;
+    private Calendar mCal = Calendar.getInstance();
+    private int mYear, mMonth;
+    private MonthType mCurrentType = MonthType.current;
+
+    private OnDataChangeListener mListener;
+
+    public enum MonthType {
+        before, current, after
+    }
 
     public CalendarView(Context context) {
         super(context);
         initWidget(context);
+        initData(context);
     }
 
     public CalendarView(Context context, AttributeSet attrs) {
         super(context, attrs);
         initWidget(context);
+        initData(context);
     }
 
     private void initWidget(Context context) {
-        mInflater = LayoutInflater.from(context);
-        View view = mInflater.inflate(R.layout.view_calendar_layout, null);
+        this.setFocusable(true);
+        View view = LayoutInflater.from(context).inflate(R.layout.view_calendar_layout, this, true);
+        mTvCalendarDate = (TextView) view.findViewById(R.id.tv_calendar_date);
         mGridView = (GridView) view.findViewById(R.id.gv_calendar);
-        mAdapter = new CalendarAdapter();
+        mBtnLeft = (ImageButton) view.findViewById(R.id.btn_left_arrow);
+        mBtnRight = (ImageButton) view.findViewById(R.id.btn_right_arrow);
+        mBtnLeft.setOnClickListener(this);
+        mBtnRight.setOnClickListener(this);
+
+    }
+
+    private void initData(Context context) {
+        mAdapter = new CalendarAdapter(context);
         mGridView.setAdapter(mAdapter);
+        switchCalendar();
+        boolean[] select = new boolean[30];
+        for (int i = 0; i < 30; i++) {
+            if (i % 2 == 0) {
+                select[i] = true;
+            } else {
+                select[i] = false;
+            }
+        }
+        setCalendarData(mYear, mMonth, select);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mBtnLeft) {
+            mCurrentType = MonthType.before;
+            switchCalendar();
+        } else if (v == mBtnRight) {
+            mCurrentType = MonthType.after;
+            switchCalendar();
+        }
+
+        if (mListener != null) {
+            mListener.dataChange(mYear, mMonth);
+        }
+    }
+
+    private void switchCalendar() {
+        int[] data;
+        switch (mCurrentType) {
+            case before:
+                data = ToolUtil.getPreMonth(mYear, mMonth);
+                mYear = data[0];
+                mMonth = data[1];
+                break;
+            case current:
+                mYear = mCal.get(Calendar.YEAR);
+                mMonth = mCal.get(Calendar.MONTH) + 1;
+                break;
+            case after:
+                data = ToolUtil.getNextMonth(mYear, mMonth);
+                mYear = data[0];
+                mMonth = data[1];
+                break;
+        }
     }
 
     public void setCalendarData(int year, int month, boolean[] select) {
         Calendar cal = Calendar.getInstance();
-        cal.set(year, month, 1);
+        cal.setTimeZone(TimeZone.getTimeZone("GMT+8:00"));
+        cal.set(year, month - 1, 1);
         int preMonth = (month - 1) > 0 ? (month - 1) : 12;
         int preYear = (month - 1) > 0 ? year : year - 1;
         int preMonthNum = ToolUtil.getDateNumByMonth(preYear, preMonth);
@@ -56,15 +125,24 @@ public class CalendarView extends FrameLayout {
         int firstPosition = week - 1;
         int minRow = (currentMonthNum + firstPosition) % 7;
         int row = (minRow == 0 ? 0 : 1) + (currentMonthNum + firstPosition) / 7;
-        int firstNum = preMonthNum - firstPosition;
-        Log.e("setData","row="+row+",firstNum="+firstNum+",firstPosition="+firstPosition+",currentMonthNum="+currentMonthNum);
-        mAdapter.setData(row, firstNum, firstPosition, currentMonthNum,select);
-        this.postInvalidate();
+        int firstNum = (firstPosition == 0) ? 1 : (preMonthNum - firstPosition + 1);
+        if (select == null || select.length != currentMonthNum) {
+            select = null;
+        }
+        String mon = month > 9 ? String.valueOf(month) : "0" + month;
+        mTvCalendarDate.setText(year + "年" + mon + "月");
+        mAdapter.setData(row, firstNum, firstPosition, currentMonthNum, select);
     }
 
+
     private class CalendarAdapter extends BaseAdapter {
+        private LayoutInflater mInflater;
         private boolean[] mSelected;
         private int mRow, mFirstNum, mFirstPosition, mMaxDay;
+
+        public CalendarAdapter(Context context) {
+            mInflater = LayoutInflater.from(context);
+        }
 
         public void setData(int row, int firstNum, int firstPosition, int maxDay, boolean[] select) {
             mRow = row;
@@ -96,6 +174,7 @@ public class CalendarView extends FrameLayout {
             if (convertView == null) {
                 holder = new ViewHolder();
                 convertView = mInflater.inflate(R.layout.view_calendar_item, parent, false);
+                holder.mBgSelect = convertView.findViewById(R.id.bg_selected);
                 holder.mIvSelect = convertView.findViewById(R.id.iv_check);
                 holder.mTvCalDay = (TextView) convertView.findViewById(R.id.tv_calendar_day);
                 convertView.setTag(holder);
@@ -110,13 +189,40 @@ public class CalendarView extends FrameLayout {
                 holder.mTvCalDay.setText(String.valueOf(position - mFirstPosition + 1));
             }
 
+            if (position < mFirstPosition || position >= mMaxDay + mFirstPosition) {
+                String text = position < mFirstPosition ? String.valueOf(mFirstNum + position) : String.valueOf(position - mMaxDay - mFirstPosition + 1);
+                holder.mTvCalDay.setText(text);
+                holder.mTvCalDay.setTextColor(getResources().getColor(R.color.color_gray));
+            } else {
+                holder.mTvCalDay.setText(String.valueOf(position - mFirstPosition + 1));
+                holder.mTvCalDay.setTextColor(getResources().getColor(R.color.color_black));
+            }
+
             if (mSelected != null) {
-                if (mSelected[position - mFirstPosition]) {
-                    holder.mIvSelect.setVisibility(View.VISIBLE);
-                    convertView.setBackgroundResource(R.drawable.circle_red_shape);
+                if (position >= mFirstPosition && position < mMaxDay + mFirstPosition) {
+                    if (mSelected[position - mFirstPosition]) {
+                        holder.mIvSelect.setVisibility(View.VISIBLE);
+                        holder.mBgSelect.setVisibility(View.VISIBLE);
+                        holder.mTvCalDay.setTextColor(Color.WHITE);
+                    } else {
+                        holder.mIvSelect.setVisibility(View.GONE);
+                        holder.mBgSelect.setVisibility(View.GONE);
+                        holder.mTvCalDay.setTextColor(getResources().getColor(R.color.color_black));
+                    }
                 } else {
                     holder.mIvSelect.setVisibility(View.GONE);
-                    convertView.setBackgroundColor(Color.TRANSPARENT);
+                    holder.mBgSelect.setVisibility(View.GONE);
+                    holder.mTvCalDay.setTextColor(getResources().getColor(R.color.color_gray));
+                }
+            } else {
+                if (position >= mFirstPosition && position < mMaxDay + mFirstPosition) {
+                    holder.mIvSelect.setVisibility(View.GONE);
+                    holder.mBgSelect.setVisibility(View.GONE);
+                    holder.mTvCalDay.setTextColor(getResources().getColor(R.color.color_black));
+                } else {
+                    holder.mIvSelect.setVisibility(View.GONE);
+                    holder.mBgSelect.setVisibility(View.GONE);
+                    holder.mTvCalDay.setTextColor(getResources().getColor(R.color.color_gray));
                 }
             }
 
@@ -125,7 +231,15 @@ public class CalendarView extends FrameLayout {
 
         class ViewHolder {
             TextView mTvCalDay;
-            View mIvSelect;
+            View mIvSelect, mBgSelect;
         }
+    }
+
+    public void setOnDataChangeListener(OnDataChangeListener listener) {
+        mListener = listener;
+    }
+
+    public interface OnDataChangeListener {
+        void dataChange(int year, int month);
     }
 }
