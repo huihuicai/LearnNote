4a869ab8edb57b510f73a4f2ec9417e65fd387a1
unknown <wanghui@foundercomics.com>
2016-04-16 22:48:51 +0800

    悬浮的效果

--- app/src/main/java/com/note/learn/fragment/FavoriteFragment.java
+++ app/src/main/java/com/note/learn/fragment/FavoriteFragment.java
@@ -2,19 +2,29 @@ package com.note.learn.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.note.learn.R;
 
 /**
  * Created by wanghui on 2016/3/31.
  */
 public class FavoriteFragment extends Fragment {
 
+    private TabLayout mTabLayout;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_favorite,container,false);
+        mTabLayout = (TabLayout) view.findViewById(R.id.tabs);
+        mTabLayout.setLayoutMode(TabLayout.MODE_FIXED);
+        mTabLayout.addTab(mTabLayout.newTab().setText("详情"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("资源"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("评论"));
+        return view;
     }
 }
