961c1f1a5e51b4b9d5b2051e11ab8b0be442681c
huihuicai <1270174074@qq.com>
2017-06-09 18:17:54 +0800

    更换透明状态栏的库

--- app/src/main/java/com/note/learn/utils/ScreenUtil.java
+++ app/src/main/java/com/note/learn/utils/ScreenUtil.java
@@ -7,8 +7,6 @@ import android.os.Build;
 import android.util.DisplayMetrics;
 import android.view.WindowManager;
 
-import com.readystatesoftware.systembartint.SystemBarTintManager;
-
 import qiu.niorgai.StatusBarCompat;
 
 /**
@@ -52,28 +50,14 @@ public class ScreenUtil {
     /**
      * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
      */
-    public static int px2dip(Context context, float pxValue) {
+    public static int px2dp(Context context, float pxValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
         return (int) (pxValue / scale + 0.5f);
     }
 
     /**
      * 沉浸式的状态栏
-     * @param activity
-     * @param resColorId
-     * @param color
      */
-    public static void setSystemBarTint(Activity activity, int resColorId, int color) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-            SystemBarTintManager tintManager = new SystemBarTintManager(activity);
-            int barColor = (resColorId > 0) ? activity.getResources().getColor(resColorId) : color;
-            tintManager.setStatusBarTintColor(barColor);
-            tintManager.setStatusBarTintEnabled(true);
-        }
-    }
-
     public static void initSystemBar(Activity activity){
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             StatusBarCompat.translucentStatusBar(activity, true);
